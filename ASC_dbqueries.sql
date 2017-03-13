cf8e4187100400308a13c5c714e4835da058e7a6
claralinanda <clinanda1@babson.edu>
2017-03-13 14:31:30 -0400

    added query with aggregate function

--- ASC_dbqueries.sql
+++ ASC_dbqueries.sql
@@ -1,18 +1,22 @@
 ﻿/*You have to create the following queries. For each query, you need to use comment block to describe what data you are looking for. Below are requirements for each query:*/
-use Soylent;
+
+USE Soylent;
 GO
-Select * From Customers; 
-Select * From Ingredients; 
-Select * From Orders; 
-Select * From Packages; 
-Select * From Product; 
-Select * From ProductIngredient; 
-Select * From Subscription;
-Select * From Suppliers;  
 
 /*Multiple SELECT * queries to display all the records from each table. (Suppose the database has 4 tables, you should create 4 queries for this requirement.)*/
 
+SELECT *
+FROM Product;
+
+SELECT 
+
+
 /*A query that uses aggregate function.*/
+
+
+SELECT sum(TotalAmount) as SumOfTotalAmount, OrderDate
+FROM Orders
+GROUP BY OrderDate;
 
 /*A query that selects records from two (or multiple) tables using INNER JOIN.*/
 
