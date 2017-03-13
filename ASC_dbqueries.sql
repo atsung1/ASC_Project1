362c32f66813cc1522bf42d9c175134d064bb5d9
Angela Tsung <atsung1@babson.edu>
2017-03-13 13:57:13 -0400

    Created Queries File

---
+++ ASC_dbqueries.sql
@@ -0,0 +1,13 @@
+﻿/*You have to create the following queries. For each query, you need to use comment block to describe what data you are looking for. Below are requirements for each query:*/
+
+/*Multiple SELECT * queries to display all the records from each table. (Suppose the database has 4 tables, you should create 4 queries for this requirement.)*/
+
+/*A query that uses aggregate function.*/
+
+/*A query that selects records from two (or multiple) tables using INNER JOIN.*/
+
+/*A query that selects records from two (or multiple) tables using LEFT OUTER JOIN (or RIGHT OUTER JOIN or FULL OUTER JOIN). Specify why outer join is neccesary in comment block. Make sure your fake data could reflect the necessary. If it could not, you need to go back to part 2 to redo the data insertion, or you can use INSERT INTO, UPDATE or DELETE statements to modify the existing data.*/
+
+/*A query that uses subquery.*/
+
+/*Add team name and names of team members in comment block.*/
